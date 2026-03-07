db96041a3f435b169adee3e694951f57028fceac
Ramazan Isanbaev <ramazanisanbaev@ya.ru>
2026-03-08 00:57:28 +0300

    Добавил файл test.go и отредактировал строку 15 в файле main.go

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func main() {
 	//var Price2 = 1200
 	//var availability2 = false
 
-	fmt.Printf("Товар: %s\nЦена: %d руб.\nВ наличии: %v\nНДС: %d%%\n\n", Tovar1, Price1, availability1, NDS)
+	fmt.Printf("Товар: %s\nЦена: %d руб.\nВ наличxczии: %v\nНДС: %d%%\n\n", Tovar1, Price1, availability1, NDS)
 	//fmt.Printf("Товар: %s\nЦена: %d руб.\nВ наличии: %v\nНДС: %d%%", Tovar2, Price2, availability2, NDS)
 }
