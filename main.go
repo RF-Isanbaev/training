bada0ccfb1ef912b045e5d1a10fc5a69ae31ba69
Ramazan Isanbaev <isanbaevramazan@ya.ru>
2026-03-08 00:46:44 +0300

    Добавил заново файл main.go

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ func main() {
 	var Price1 = 45000
 	var availability1 = false
 	var NDS = 20
-	var Tovar2 = "Мышь"
-	var Price2 = 1200
-	var availability2 = false
+	//var Tovar2 = "Мышь"
+	//var Price2 = 1200
+	//var availability2 = false
 
 	fmt.Printf("Товар: %s\nЦена: %d руб.\nВ наличии: %v\nНДС: %d%%\n\n", Tovar1, Price1, availability1, NDS)
-	fmt.Printf("Товар: %s\nЦена: %d руб.\nВ наличии: %v\nНДС: %d%%", Tovar2, Price2, availability2, NDS)
+	//fmt.Printf("Товар: %s\nЦена: %d руб.\nВ наличии: %v\nНДС: %d%%", Tovar2, Price2, availability2, NDS)
 }
