9c7298a15e4702b9853c015ee1903e900089c38e
Ramazan Isanbaev <isanbaevramazan@ya.ru>
2026-03-09 01:34:17 +0300

    Revert "deleted main"
    This reverts commit 45db5b17944714c81443b566414d953dcaa7409f.

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	x := 0
+	a := 5
+	b := 0
+	for {
+		x = +1
+		b = x
+		text := (a + b) / 2
+		fmt.Printf("Среднее число между 5 и (от 0 до 30) : %d\n", text)
+		time.Sleep(1 * time.Second)
+	}
+
+}
